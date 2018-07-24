98329ab0687efa42daad262c9da9f42f2855bcae
Gabor Schmied <schmied.gabor@gmail.com>
2018-07-24 09:28:18 +0200

    added forms

--- src/app/events/create-event.component.ts
+++ src/app/events/create-event.component.ts
@@ -1,26 +1,34 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { IEvent, EventService } from './shared';
+import { TouchSequence } from '../../../node_modules/@types/selenium-webdriver';
 
 @Component({
-    template: `
-        <h1>New Event</h1>
-        <hr>
-        <div class="col-md-6">
-            <h3>[Create Event Form]</h3>
-            <br />
-            <br />
-            <button type="submit" class="btn btn-primary">Save</button>
-            <button type="submit" class="btn btn-default" (click)="cancel()">Cancel</button>
-        </div>
-    `
+	templateUrl: 'create-event.component.html',
+	styles: [`
+		em { float: right; color: #e05c65; padding-left: 10px; }
+		.error input { background-color: #e3c3c5; }
+		.error ::-webkit-input-placeholder { color: #999; }
+		.error ::-moz-placeholder { color: #999; }
+		.error :-moz-placeholder { color: #999; }
+		.error :-ms-input-placeholder { color: #999; }
+	`]
 })
 export class CreateEventComponent {
-    isDirty: boolean = true;
-    constructor(private router: Router) {
+	newEvent: IEvent;
+	isDirty: boolean = true;
 
-    }
+	constructor(private router: Router, private eventService: EventService) {
 
-    cancel() {
-        this.router.navigate(['/events']);
-    }
+	}
+
+	saveEvent(formValues) {
+		this.eventService.saveEvent(formValues);
+		this.isDirty = false;
+		this.router.navigate(['/events']);
+	}
+
+	cancel() {
+		this.router.navigate(['/events']);
+	}
 }
