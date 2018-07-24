1a964a63d246a32e36a4d8dfaad3be0457460347
Gabor Schmied <schmied.gabor@gmail.com>
2018-07-24 14:07:36 +0200

    added filtering to sessions' list

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,14 +1,32 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { ISession } from '../shared';
 
 @Component({
 	selector: 'session-list',
 	templateUrl: './session-list.component.html',
 })
-export class SessionListComponent implements OnInit {
+export class SessionListComponent implements OnInit, OnChanges {
 	@Input() sessions: ISession[];
+	@Input() filterBy: string;
+	visibleSessions: ISession[] = [];
 
 	constructor() { }
 
 	ngOnInit(): void { }
+
+	ngOnChanges(): void {
+		if (!this.sessions) {
+			return;
+		}
+
+		this.filterSessions(this.filterBy);
+	}
+
+	filterSessions(filter: string) {
+		if (filter === 'all') {
+			this.visibleSessions = this.sessions.slice(0);
+		} else {
+			this.visibleSessions = this.sessions.filter(s => s.level.toLocaleLowerCase() === filter);
+		}
+	}
 }
