6ea263da66c94513bac91e77a71a208c69270812
Gabor Schmied <schmied.gabor@gmail.com>
2018-07-25 18:32:11 +0200

    closing modal dialog with @ViewChlid in SimpleModalComponent

--- src/app/common/simple-modal.component.ts
+++ src/app/common/simple-modal.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, Inject } from '@angular/core';
+
+import { JQ_TOKEN } from './jquery.service'
 
 @Component({
 	selector: 'simple-modal',
 	template: `
-		<div id="{{elementId}}" class="modal fade" tabindex="-1" role="dialog">
+		<div id="{{elementId}}" #modalcontainer class="modal fade" tabindex="-1" role="dialog">
 			<div class="modal-dialog">
 				<div class="modal-content">
 					<div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
 						<h4 class="modal-title">{{title}}</h4>
 					</div>
-					<div class="modal-body">
+					<div class="modal-body" (click)="closeModal()">
 						<ng-content></ng-content>
 					</div>
 				</div>
@@ -24,8 +26,13 @@ import { Component, OnInit, Input } from '@angular/core';
 export class SimpleModalComponent implements OnInit {
 	@Input() title: string;
 	@Input() elementId: string;
+	@ViewChild('modalcontainer') containerEl: ElementRef;
 
-	constructor() { }
+	constructor(@Inject(JQ_TOKEN) private $: any) { }
 
 	ngOnInit(): void { }
+
+	closeModal()  {
+		this.$(this.containerEl.nativeElement).modal('hide');
+	}
 }
