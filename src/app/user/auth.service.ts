50cca8f1a52e436e82893c491ee062b7ec54478b
gabor182 <schmied.gabor@gmail.com>
2018-08-07 10:21:56 +0200

    logging in using http methods

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,26 +1,45 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { tap, catchError } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 
 import { IUser } from './user.model';
 
+
+
 @Injectable()
 export class AuthService {
-    currentUser: IUser;
+	currentUser: IUser;
 
-    loginUser(userName: string, password: string) {
-        this.currentUser = {
-            id: 1,
-            userName: userName,
-            firstName: 'John',
-            lastName: 'Papa',
-        }
-    }
+	constructor(private http: HttpClient) { }
 
-    updateCurrentUser(firstName: string, lastName: string) {
-        this.currentUser.firstName = firstName;
-        this.currentUser.lastName = lastName;
+	loginUser(userName: string, password: string) {
+		let loginInfo = { username: userName, password: password }
+		let options = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+
+		return this.http.post('/api/login', loginInfo, options)
+			.pipe(tap(data => { // called when response is available
+				this.currentUser = <IUser>data['user']; // <IUser> casts data['user'] to IUser type
+			}))
+			.pipe(catchError(err => {
+				return of(false);
+			}));
 	}
 
-    isAuthenticated(): boolean {
-        return !!this.currentUser;
-    }
+	updateCurrentUser(firstName: string, lastName: string) {
+		this.currentUser.firstName = firstName;
+		this.currentUser.lastName = lastName;
+	}
+
+	isAuthenticated(): boolean {
+		return !!this.currentUser;
+	}
+
+	private handleError<T>(operation = 'operation', result?: T) {
+		return (error: any): Observable<T> => {
+			console.error(error);
+			return of(result as T);
+		}
+	}
 }
--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -3,25 +3,32 @@ import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 
 @Component({
-    templateUrl: './login.component.html',
-    styles: [`
-        em { float: right; color: #e05c65; padding-left: 10px; }
-    `]
+	templateUrl: './login.component.html',
+	styles: [`
+		em { float: right; color: #e05c65; padding-left: 10px; }
+	`]
 })
 export class LoginComponent {
 
-    userName: string;
-    password: string;
-    mouseoverLogin: boolean;
+	userName: string;
+	password: string;
+	mouseoverLogin: boolean;
+	loginInvalid = false;
 
-    constructor(private authService: AuthService, private router: Router) { }
+	constructor(private authService: AuthService, private router: Router) { }
 
-    login(values) {
-        this.authService.loginUser(values.userName, values.password);
-        this.router.navigate(['events']);
-    }
+	login(values) {
+		this.authService.loginUser(values.userName, values.password).subscribe(response => {
+			if (!response) {
+				this.loginInvalid = true;
+			} else {
+				this.loginInvalid = false;
+				this.router.navigate(['events']);
+			}
+		});
+	}
 
-    cancel() {
-        this.router.navigate(['events']);
-    }
+	cancel() {
+		this.router.navigate(['events']);
+	}
 }
